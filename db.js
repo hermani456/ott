e93b450ef29ff6722935af89b4aec28717e2563a
hermani456 <hermani457@gmail.com>
2022-07-31 23:53:01 -0400

    update

--- db.js
+++ db.js
@@ -2,12 +2,21 @@ require('dotenv').config()
 const { Pool } = require('pg')
 
 const pool = new Pool({
-   user: process.env.POSTGRESS_USER,
-   host: process.env.POSTGRESS_HOST,
-   database: process.env.POSTGRESS_DATABASE,
-   password: process.env.POSTGRESS_PASSWORD,
-   port: process.env.POSTGRESS_PORT,
-   sslmode: process.env.POSTGRESS_SSLMODE,
+   databaseurl: process.env.DATABASE_URL,
+   user: process.env.PGUSER,
+   host: process.env.PGHOST,
+   database: process.env.PGDATABASE,
+   password: process.env.PGPASSWORD,
+   port: process.env.PGPORT,
 })
+
+// const pool = new Pool({
+//    user: process.env.POSTGRESS_USER,
+//    host: process.env.POSTGRESS_HOST,
+//    database: process.env.POSTGRESS_DATABASE,
+//    password: process.env.POSTGRESS_PASSWORD,
+//    port: process.env.POSTGRESS_PORT,
+//    sslmode: process.env.POSTGRESS_SSLMODE,
+// })
 
 module.exports = pool;
