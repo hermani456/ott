aeb7e752b7a5dbae2d94e8eaceffe35d590999d3
hermani456 <hermani457@gmail.com>
2022-07-31 22:23:32 -0400

    update

--- app.js
+++ app.js
@@ -8,9 +8,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-	res.send('Hello World!')
-})
+app.get('/', otGet)
 app.post('/', otPost)
 app.put('/', otPut)
 app.delete('/', otDelete)
