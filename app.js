b8fc7eff90555af56094b83bf9c4cedf94970b76
hermani456 <hermani457@gmail.com>
2022-08-02 22:39:12 -0400

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const port = 3000
+const port = 5000
 const { otGet, otPost, otPut, otDelete } = require('./functions')
 
 app.use(cors())
