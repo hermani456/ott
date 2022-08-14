f405e8f706d92566d19e487a81096beb4925f9b6
hermani456 <hermani457@gmail.com>
2022-08-14 01:56:20 -0400

    update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const port = process.env.PORT || 5000
 const { otGet, otPost, otPut, otDelete, botPut, botGet } = require('./functions')
 
-app.use(cors( { origin: process.env.CORS_ORIGIN } ))
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
