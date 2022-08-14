2dd3bd4da1ab5ec0a98a90cd2e8725f8e8f5617b
hermani456 <hermani457@gmail.com>
2022-08-14 01:48:09 -0400

    update

--- db.js
+++ db.js
@@ -1,14 +1,6 @@
 require('dotenv').config()
 const { Pool } = require('pg')
 
-// const pool = new Pool({
-//    user: 'postgres',
-//    host: 'containers-us-west-89.railway.app',
-//    database: 'railway',
-//    password: '29um36Zy5I75VBDTfJuN',
-//    port: '5614'
-// })
-
 const pool = new Pool({
    user: process.env.POSTGRESS_USER,
    host: process.env.POSTGRESS_HOST,
