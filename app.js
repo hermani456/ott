ba97ee1f9dd1fcc74614908d8b9d163c193852cc
hermani456 <hermani456@gmail.com>
2022-10-31 19:23:29 -0300

    update

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use("/auth", require("./routes/auth"));
 app.use("/refresh", require("./routes/refresh"));
 app.use("/logout", require("./routes/logout"));
 
-app.use(verifyJwt);
+// app.use(verifyJwt);
 app.use("/api/v1", require("./routes/api"));
 
 app.get("/api/v1/entregados", entregadosGet);
