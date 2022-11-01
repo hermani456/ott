772d98e7b6e5e6a559b75d78d34b21b53afb686f
hermani456 <hermani456@gmail.com>
2022-11-01 00:23:16 -0300

    update

--- middleware/logger.js
+++ middleware/logger.js
@@ -4,9 +4,9 @@ const { v4: uuid } = require('uuid');
 const fs = require('fs');
 const fsPromises = require('fs').promises;
 const path = require('path');
+const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
 
 const logEvents = async (message, logName) => {
-    const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
     const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
 
     try {
