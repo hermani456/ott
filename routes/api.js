1da84e2fc19dddeebc77a9e64a48bb0337e8a4dd
hermani456 <98556924+hermani456@users.noreply.github.com>
2022-11-02 09:36:33 -0300

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -4,11 +4,12 @@ const { otGet, otPost, otPut, otDelete } = require("../controllers/functions");
 // const {verifyJwt} = require('../middleware/verifyJwt') protect routes
 const roles_list = require("../config/roles_list");
 const verifyRoles = require("../middleware/verifyRoles");
+// .post(verifyRoles(roles_list.admin), otPost)
 
 router
   .route("/")
   .get(otGet)
-  .post(verifyRoles(roles_list.admin), otPost)
+  .post(otPost)
   .put(otPut)
   .delete(otDelete);
 
